1ae1677e27bebaa82fd4ecf5e0e9cb8924bf2a2a
DanilKlochkov <danil.klochkov.92@bk.ru>
2022-11-29 18:16:49 +0300

    update tests

--- src/test/java/com/example/testsonarproject/SomeClassTests.java
+++ src/test/java/com/example/testsonarproject/SomeClassTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.Assert.*;
 
 @SpringBootTest
-public class SomeServiceTest {
+public class SomeClassTests {
 
     @Test
     public void plusTest() {
