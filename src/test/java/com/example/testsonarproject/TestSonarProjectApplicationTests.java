ad0bb3cd2a1872fba670633e6651347672073a6a
DanilKlochkov <danil.klochkov.92@bk.ru>
2022-11-29 18:44:13 +0300

    update tests

--- src/test/java/com/example/testsonarproject/TestSonarProjectApplicationTests.java
+++ src/test/java/com/example/testsonarproject/TestSonarProjectApplicationTests.java
@@ -1,14 +1,38 @@
 package com.example.testsonarproject;
 
-import org.junit.jupiter.api.Test;
+import com.example.testsonarproject.service.SomeClass;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class TestSonarProjectApplicationTests {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
-    @Test
-    void contextLoads() {
-        System.out.println(1+1);
+@SpringBootTest
+public class TestSonarProjectApplicationTests {
+
+    @org.junit.Test
+    public void plusTest() {
+        assertEquals(2, SomeClass.plus(1,1).intValue());
+    }
+
+    @org.junit.Test
+    public void minusTest() {
+        assertEquals(2, SomeClass.minus(3,1).intValue());
+    }
+
+    @org.junit.Test
+    public void multiplyTest() {
+        assertEquals(2, SomeClass.multiply(2,1).intValue());
+    }
+
+    @org.junit.Test
+    public void divideTest() {
+        assertEquals(2, SomeClass.divide(4,2).intValue());
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void divideTestWithError() {
+        assertNull(SomeClass.divide(1,0));
     }
 
 }
