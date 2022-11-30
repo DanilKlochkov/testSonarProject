1e5be334428673af3de0dd3e707539f4632e64ab
DanilKlochkov <danil.klochkov.92@bk.ru>
2022-11-30 11:14:13 +0300

    update tests

--- src/test/java/com/example/testsonarproject/TestSomeClassTests.java
+++ src/test/java/com/example/testsonarproject/TestSomeClassTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class TestSomeClassTests {
+class TestSomeClassTests {
     
     @Test
     public void plusTest() {
